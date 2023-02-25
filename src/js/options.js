1fef34a1eaf90b7520f942a8eee30781ab3c0c8e
Wil adeilt Langford <adeilt.wsl@gmail.com>
2023-02-25 14:52:52 -0800

    Import and export settings in JSON format.

--- src/js/options.js
+++ src/js/options.js
@@ -20,26 +20,32 @@ function load_data(){
 		let colors = response["colors"] ? response["colors"] : {};
 		let user_tags = response["user_tags"] ? response["user_tags"] : {};
 
-		console.log(colors);
-
-		// Options
-		// no non-color, non-tag options yet
-
-		// Colors
+		// COLORS
+		// if a color preference is unset, set it to default
 		for (const color_name in color_defaults) {
+			let colors_defaulted = 0;
 			if (!(color_name in colors)) {
+				colors_defaulted++;
+				console.log("Miasmatic: using default color for: " + color_name);
 				colors[color_name] = color_defaults[color_name];
+			}
+			if (colors_defaulted) {
+				chrome.storage.sync.set({"colors": color_defaults});
 			}
 		}
 
+		// configure color UI elements with stored preferences
 		for (const color_name in color_defaults) {
 			document.getElementById(color_name).value = colors[color_name];
 		}
-		let list_div = document.getElementById("tag_list_div");
 
-		// Tags
+		// TAGS
+		let tag_count = 0;
+		let list_div = document.getElementById("tag_list_div");
 		for (const tag_name in user_tags) {
-			console.log("processing tag: " + tag_name);
+			// console.log("Miasmatic: processing tag: " + tag_name);
+			tag_count++;
+
 			let tag = user_tags[tag_name];
 
 			let tag_span = document.createElement('span');
@@ -58,8 +64,44 @@ function load_data(){
 			list_div.appendChild(tag_span);
 			list_div.appendChild(document.createElement('br'));
 		}
+		console.log("Miasmatic: loaded " + tag_count + " tags.")
 
+		// OPTIONS
+		// no non-color, non-tag options yet
+
+		// SETTINGS TRANSPORT
+		export_settings();
 	});
+}
+
+function export_settings() {
+	chrome.storage.sync.get(["user_tags", "options", "colors"], function(response){
+		let options = response["options"] ? response["options"] : {};
+		let colors = response["colors"] ? response["colors"] : {};
+		let user_tags = response["user_tags"] ? response["user_tags"] : {};
+
+		document.getElementById("settings_export_text").value = JSON.stringify({
+			options: options,
+			colors: colors,
+			user_tags: user_tags,
+		})
+	});
+}
+
+function display_error(target_id, message, timeout=5000) {
+	let target = document.getElementById(target_id);
+	if (target.classList.contains('error_span')) {
+		target.innerText = message;
+		setTimeout(() => { target.innerText = ""; }, timeout);
+	}
+}
+
+function display_message(target_id, message, timeout=5000) {
+	let target = document.getElementById(target_id);
+	if (target.classList.contains('message_span')) {
+		target.innerText = message;
+		setTimeout(() => { target.innerText = ""; }, timeout);
+	}
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -105,11 +147,71 @@ document.addEventListener("DOMContentLoaded", function () {
 				document.getElementById("reset_colors").disabled = true;
 
 				for (const color_name in color_defaults) {
-					console.log(color_name + "=" + color_defaults[color_name]);
 					document.getElementById(color_name).value = color_defaults[color_name];
 				}
 
 				chrome.storage.sync.set({"colors": color_defaults});
+			});
+		}
+
+		if (inp.id == "confirm_import_settings") {
+			inp.addEventListener("change", function() {
+				document.getElementById("import_settings").disabled = !inp.checked;
+			});
+		}
+
+		if (inp.id == "import_settings") {
+			inp.addEventListener("click", function() {
+				console.log("Importing settings!")
+				let imported_settings = "BAD";
+				try {
+					imported_settings = JSON.parse(document.getElementById('settings_import_text').value);
+				} catch (e) {
+					if (e instanceof SyntaxError) {
+						console.log("BAD");
+						display_error('import_error', "Error parsing settings data as JSON!");
+					}
+				}
+				if (imported_settings != "BAD") {
+					console.log("PARSED");
+					if (!('colors' in imported_settings) ||
+						!('user_tags' in imported_settings) ||
+						!('options' in imported_settings)) {
+
+						console.log("MISSING");
+						console.log(imported_settings);
+						display_error('import_error', "Missing required items in parsed settings data.");
+					} else {
+						console.log('IMPORTING');
+						console.log(imported_settings);
+						let imported = (
+							Object.keys(imported_settings.user_tags).length + " tags," +
+							Object.keys(imported_settings.colors).length + " colors," +
+							Object.keys(imported_settings.options).length + " other options"
+						);
+						console.log("Importing: " + imported);
+
+						chrome.storage.sync.set({
+							colors: imported_settings.colors,
+							user_tags: imported_settings.user_tags,
+							options: imported_settings.options,
+						}).then(() => {
+							load_data ();
+							display_message('import_message', "Imported: " + imported, 10000);
+						});
+					}
+				}
+
+				document.getElementById("confirm_import_settings").checked = false;
+				document.getElementById("import_settings").disabled = true;
+				document.getElementById("settings_import_text").value = "";
+			});
+		}
+
+		if (inp.id == "copy_exported_settings") {
+			inp.addEventListener("click", function () {
+				navigator.clipboard.writeText(document.getElementById('settings_export_text').value);
+				display_message('export_message', "Settings copied.")
 			});
 		}
 
